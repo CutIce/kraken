db5a0ca50393b725490a41532a4e8d2349897a82
CutIce <1031941295@qq.com>
2020-04-16 14:59:53 +0800

    2020-4-16

---
+++ func.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hsh on 2020/4/16.
+//
+
+#include "func.h"
---
+++ func.h
@@ -0,0 +1,14 @@
+//
+// Created by hsh on 2020/4/16.
+//
+
+#ifndef KRAKEN_FUNC_H
+#define KRAKEN_FUNC_H
+
+
+class func {
+
+};
+
+
+#endif //KRAKEN_FUNC_H
