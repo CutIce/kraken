424f05f05df1eefddf0d80c60172a1e32e6036de
CutIce <1031941295@qq.com>
2020-05-07 00:01:30 +0800

    repair the bug about decreaseKey() before findMin()

--- ClionCode/main.cpp
+++ ClionCode/main.cpp
@@ -1,6 +1,28 @@
+#include "priorityQueue.h"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int * data=new int [n];
+    for (int i=0;i<n;++i){
+        cin>>data[i];}
+    priorityQueue<int> pqueue(data,n);
+
+    int x;          cin>>x;
+    int value;      cin>>value;
+
+    int i;          i=pqueue.findMin(x);
+    if (i!=-1){
+    pqueue.print();  cout<<endl;
+    pqueue.decreaseKey(i,value);
+    pqueue.print();  cout<<endl;
+
+    while (! pqueue.isEmpty()){
+        cout<<pqueue.deQueue()<<" ";
+    }}
+
     return 0;
 }
